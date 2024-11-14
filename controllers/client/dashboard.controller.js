2c19ab92e8094df3c12ff3d61ad615b3283e53fb
Truong Hoang Duy <truonghoangduy2612@gmail.com>
2024-11-14 20:38:11 +0700

    add dashboard

--- controllers/client/dashboard.controller.js
+++ controllers/client/dashboard.controller.js
@@ -1,3 +1,7 @@
 module.exports.index = (req, res) => {
   res.send("Day la dashboard");
 };
+
+module.exports.members = (req, res) => {
+  res.render("client/pages/dashboard/members", { title: "Tìm kiếm đoàn viên" });
+};
--- routes/client/dashboard.route.js
+++ routes/client/dashboard.route.js
@@ -2,4 +2,5 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/client/dashboard.controller.js");
 router.get("/", controller.index);
+router.get("/dashboard/members", controller.members);
 module.exports = router;
